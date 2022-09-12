56d79b39b5471b2309b4c58e1ed747e1190e8aef
chebihiF <chebihi.f@ucd.ac.ma>
2022-09-12 13:39:16 +0100

    add restaurant dummy data

--- app/src/main/java/org/doronco/restaurantapp/MainActivity.kt
+++ app/src/main/java/org/doronco/restaurantapp/MainActivity.kt
@@ -3,14 +3,22 @@ package org.doronco.restaurantapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.doronco.restaurantapp.ui.theme.RestaurantAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,15 +32,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun ColoredBox() {
+    Box(modifier = Modifier
+        .size(120.dp)
+        .background(Color.Green)
+        .clip(RoundedCornerShape(size = 50.dp))
+        .background(Color.Blue)
+        .padding(16.dp)
+        .background(Color.Red)) {
 
-
-@Composable
-fun DefaultPreview() {
-    RestaurantAppTheme {
-        Greeting("Android")
     }
 }
